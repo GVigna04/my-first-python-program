37bd1e615b7ca73671f45e0ccd255ddf95594a13
vigna <vigna@vignas-Laptop.local>
2026-01-03 23:05:24 -0600

    add calculator

--- hello.py
+++ hello.py
@@ -1,5 +1,38 @@
-name = input("what is your name")
-age = input("what is your age")
+print("caluculator")
 
-print(f"hello {name}! you are {age} years old. Nice to meet you!")
-print("have a great day")
+num1=float(input("enter first number"))
+num2=float(input("enter second number"))
+
+print("Choose an operation:")
+print(" +  for addition")
+print(" -  for subtraction")
+print(" *  for multiplication")
+print(" /  for division")
+
+operation = input("enter operation")
+
+if operation == "+":
+    result = num1 + num2
+    print("result:",result)
+
+elif operation == "-":
+    result = num1 - num2
+    print("result:",result)
+
+elif operation == "*":
+    result = num1 * num2
+    print("result:",result)
+
+elif operation == "/":
+    if num2 == 0:
+       print("error: cannot divide by 0")
+    else:
+       result = num1 / num2
+       print("result:", result)
+
+else:
+    print("Invalid operation")
+
+print("thank you for using calculator")
+
+

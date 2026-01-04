fc521ae357cdc63fe2f58584d84e4958a7ecfc1a
vigna <vigna@vignas-Laptop.local>
2026-01-03 22:03:31 -0600

    adding age

--- hello.py
+++ hello.py
@@ -1,2 +1,5 @@
 name = input("what is your name")
-print("hello,",name)
+age = input("what is your age")
+
+print(f"hello {name}! you are {age} years old. Nice to meet you!")
+print("have a great day")
